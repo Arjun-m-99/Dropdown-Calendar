34423b825b68b3b1a73820287e13a8f425ebe1f0
mallikarjunaGITHUBnexgile <mallikarjuna.akunuri@nexgile.com>
2022-02-24 12:31:07 +0530

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dropdown-Calender';
+  title = 'Dropdown-Calendar';
+  public CalendarForm:any;
+  public date: Array<number> = [];
+  public month=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  public year:any[]=[]
+
+  constructor(public formBuilder: FormBuilder){}
+
+  ngOnInIt(){
+    this.calendar();
+    for(let i=1;i<=31;i++){
+      this.date[i]=i;
+    }
+    for(let i=1950;i<=2022;i++){
+      this.year[i]=i;
+    }
+  }
+  calendar(){
+    this.CalendarForm=this.formBuilder.group({
+      date:new FormControl("",Validators.required),
+      moth:new FormControl("",Validators.required),
+      year:new FormControl("",Validators.required)
+    })
+  }
+  values(){
+    for(let i=1;i<=31;i++){
+      this.date[i]=i;
+    }
+    for(let i=1950;i<=2022;i++){
+      this.year[i]=i;
+    }
+  }
+
 }
