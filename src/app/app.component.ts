22d1f7578cafa3d3201942cf321710cc64861523
mallikarjunaGITHUBnexgile <mallikarjuna.akunuri@nexgile.com>
2022-02-28 16:47:54 +0530

    formControl issue solved by using Constructor()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,52 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'Dropdown-Calendar';
-  public CalendarForm:any;
-  public date: Array<number> = [];
-  public month=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  public year:any[]=[]
+  public calendarForm:any;
+  public dates: number[]=[];
+  public months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  public years:any[]=[]
+  public startYear=1950;
+  public endYear=2022;
+  public j=0;
 
-  constructor(public formBuilder: FormBuilder){}
-
-  ngOnInIt(){
+  constructor(public formBuilder: FormBuilder){
     this.calendar();
     for(let i=1;i<=31;i++){
-      this.date[i]=i;
+      this.dates[i]=i;
     }
-    for(let i=1950;i<=2022;i++){
-      this.year[i]=i;
+    for(let i=this.startYear;i<=this.endYear;i++){
+      this.years[this.j++]=i;
     }
   }
+
+  ngOnInIt():void{
+     this.calendar();
+     console.log("ngOnInIt")
+    // for(let i=1;i<=31;i++){
+    //   this.dates[i]=i;
+    // }
+    // for(let i=1950;i<=2022;i++){
+    //   //let j=1;
+    //   this.years[++this.j]=i;
+      
+    // }
+   // while(this.startYear<=this.endYear)
+  }
   calendar(){
-    this.CalendarForm=this.formBuilder.group({
-      date:new FormControl("",Validators.required),
-      moth:new FormControl("",Validators.required),
-      year:new FormControl("",Validators.required)
+    console.log("calender");
+    this.calendarForm=this.formBuilder.group({
+      dat: new FormControl("",Validators.required),
+      month: new FormControl("",Validators.required),
+      year: new FormControl("",Validators.required),
     })
   }
   values(){
-    for(let i=1;i<=31;i++){
-      this.date[i]=i;
-    }
-    for(let i=1950;i<=2022;i++){
-      this.year[i]=i;
-    }
+    // for(let i=1;i<=31;i++){
+    //   this.dates[i]=i;
+    // }
+    // for(let i=1950;i<=2022;i++){
+    //   this.years[i]=i;
+    // }
   }
 
 }
