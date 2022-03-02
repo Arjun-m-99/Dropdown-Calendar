2cf336d0e9f91f277c19ca64aeea1c1fde3bfd72
mallikarjunaGITHUBnexgile <mallikarjuna.akunuri@nexgile.com>
2022-03-02 13:55:35 +0530

    ngOnInit() problem resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,55 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { CustomValidators } from './custom-validator';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dropdown-Calendar';
-  public calendarForm:any;
-  public dates: number[]=[];
-  public months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  public years:any[]=[]
-  public startYear=1950;
-  public endYear=2022;
-  public j=0;
+  public calendarForm: any;
+  public dates: number[] = [];
+  public months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  public years: any[] = []
+  public startYear!: number;
+  public endYear!: number;
+  public j = 0;
 
-  constructor(public formBuilder: FormBuilder){
+  constructor(public formBuilder: FormBuilder) {
+    //get year by Date Class
+    var currentTime = new Date()
+    this.startYear = currentTime.getFullYear() - 100;
+    this.endYear = currentTime.getFullYear();
+    for (var i = 1; i <= 31; i++) {
+      this.dates[this.j++] = i;
+    }
+    this.j = 0;
+    for (let i = this.startYear; i <= this.endYear; i++) {
+      this.years[this.j++] = i;
+    }
+    //this.calendar();
+  }
+
+
+  ngOnInit(): void {
     this.calendar();
-    for(let i=1;i<=31;i++){
-      this.dates[i]=i;
-    }
-    for(let i=this.startYear;i<=this.endYear;i++){
-      this.years[this.j++]=i;
-    }
-  }
+    console.log("ngOnInIt");
 
-  ngOnInIt():void{
-     this.calendar();
-     console.log("ngOnInIt")
-    // for(let i=1;i<=31;i++){
-    //   this.dates[i]=i;
-    // }
-    // for(let i=1950;i<=2022;i++){
-    //   //let j=1;
-    //   this.years[++this.j]=i;
-      
-    // }
-   // while(this.startYear<=this.endYear)
   }
-  calendar(){
+  calendar() {
     console.log("calender");
-    this.calendarForm=this.formBuilder.group({
-      date: new FormControl("",Validators.required),
-      month: new FormControl("",Validators.required),
-      year: new FormControl("",Validators.required),
-    },{validator:CustomValidators()}
+    
+    this.calendarForm = this.formBuilder.group({
+      date: new FormControl("", Validators.required),
+      month: new FormControl("", Validators.required),
+      year: new FormControl("", Validators.required),
+    }, { validator: CustomValidators() }
     )
   }
-  values(){
+  values() {
     // for(let i=1;i<=31;i++){
     //   this.dates[i]=i;
     // }
